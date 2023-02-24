766e38720fd9f07b096c736c3a6142814d66cff5
bashgame <amen.kester@gmail.com>
2023-02-24 13:29:41 -0500

    Template files created
    Added templates for unit tests, config for nose and coverage, added nose, pinnochio, coverage and pylint to requirements.txt

---
+++ tests/test_temp.py
@@ -0,0 +1,30 @@
+"""
+Template Test Cases, please update
+"""
+from unittest import TestCase
+from source import *
+
+class TestTemplate(TestCase):
+    """ Test Template, please update """
+
+    @classmethod
+    def setUpClass(cls):
+        """ Do something to set up tests, please update """
+
+    @classmethod
+    def tearDownClass(cls):
+        """ Do something to clean up tests, please update """
+
+    def setUp(self):
+        """ Ensure each test runs clean, please update """
+
+    def tearDown(self):
+        """ Ensure each test runs clean, please update """
+
+    ###########################################################################
+    #   T E S T  C A S E S
+    ###########################################################################
+
+    def test_template(self):
+        """ Template test case, please update """
+        self.assertEqual(True,False)
