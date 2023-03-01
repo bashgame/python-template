bc7a29d5990db887ee92058ddff80a2323564c4b
bashgame <122762411+bashgame@users.noreply.github.com>
2023-02-28 22:37:57 -0500

    Update test_temp.py
    Changing template test to assertTrue(False)

--- tests/test_temp.py
+++ tests/test_temp.py
@@ -27,4 +27,4 @@ class TestTemplate(TestCase):
 
     def test_template(self):
         """ Template test case, please update """
-        self.assertEqual(True,False)
+        self.assertTrue(False)
